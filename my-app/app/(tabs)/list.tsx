75e4958f7a89d0438a677b14e90dbfe39222fa5e
WTL04 <williamluu04@gmail.com>
2025-11-02 00:34:59 -0700

    listbox component and list tab scrolling feature

---
+++ my-app/app/(tabs)/list.tsx
@@ -0,0 +1,36 @@
+import { ScrollView, StyleSheet, View } from 'react-native';
+import ListBox from "../../components/ListBox";
+import data from '../../assets/data/fake_venues.json';
+
+export default function ListScreen() {
+  return (
+
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.listContainer}>
+          {/*mapping json file info into boxes*/}
+          {data.map((venue) => (
+            <ListBox
+              venue_name={venue.venue_name}
+              address={venue.address}
+              deal={venue.deals}
+            />
+          ))}
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#F4EAE1',
+  },
+
+  listContainer: {
+    marginTop: 40,
+    margin: 20,
+  }
+});

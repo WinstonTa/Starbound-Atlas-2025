9a652e7a0bb6260e371b4d9872c9287de57edb12
WTL04 <williamluu04@gmail.com>
2025-11-18 02:28:17 -0800

    installed dev client and android apk for firebase testing

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,11 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import { useEffect } from 'react';
+
 
 export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>HappyMapper First Screen</Text>
-    </View>
-  );
+  // return (
+  //   <View style={styles.container}>
+  //     <Text style={styles.text}>HappyMapper First Screen</Text>
+  //   </View>
+  // );
+
+  // debuggging firebase connection
+  useEffect(() => {
+    firestore().collection('ping').limit(1).get()
+      .then(() => console.log('Firestore OK'))
+      .catch(e => console.log('Firestore ERR', e));
+  }, []);
+  return <View styles={ styles.container }><Text>Home page, testing firebase connection, check logs</Text></View>;
 }
 
 const styles = StyleSheet.create({

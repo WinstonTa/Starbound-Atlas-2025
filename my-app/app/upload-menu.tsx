cb631a7e9dba1192bc8d66e022fb64dd1b8fa60c
WTL04 <williamluu04@gmail.com>
2025-11-18 19:16:56 -0800

    cleaning up some files and attempting live feed for list.tsx

--- my-app/app/upload-menu.tsx
+++ my-app/app/upload-menu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { View, Text, StyleSheet, Alert, ScrollView, TouchableOpacity, Image, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import VenueForm from '../components/VenueForm';
